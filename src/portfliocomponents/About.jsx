2e1de968b94f419085e467aa744a3031bd257618
MANIPANDI17 <manipandichinnapandi@gmail.com>
2025-08-13 18:07:49 +0530

    Update About.jsx

--- src/portfliocomponents/About.jsx
+++ src/portfliocomponents/About.jsx
@@ -4,11 +4,12 @@ const About = () => (
   <section id="about" className="section">
     <h2>About Me</h2>
     <p>
-I am a passionate MERN stack developer with hands-on experience in building full-stack web applications using MongoDB, Express.js, React.js, and Node.js. I enjoy crafting responsive and dynamic user interfaces while ensuring efficient backend integration. With a strong understanding of RESTful APIs, database design, and modern JavaScript, I develop scalable and maintainable solutions. I'm also familiar with version control systems like GitHub and deployment tools. I thrive in collaborative environments and continuously seek opportunities to learn and grow in the ever-evolving tech landscape. My goal is to build impactful applications that solve real-world problems and enhance user experiences.
+I am a passionate MERN stack developer with hands-on experience in building full-stack web applications using MongoDB, Express.js, React.js, and Node.js. I enjoy crafting responsive and dynamic user interfaces while ensuring efficient backend integration. With a strong understanding of RESTful APIs, database design, and modern JavaScript, I develop scalable and maintainable solutions. I'm also familiar with version control systems like GitHub and deployment tools. I thrive in collaborative environments and continuously seek opportunities to learn and grow in the ever-evolving tech landscape. My goal is to build impactful applications that solve real-world problems and enhance user experience.
 
     </p>
   </section>
 );
 
 export default About;
+
 
